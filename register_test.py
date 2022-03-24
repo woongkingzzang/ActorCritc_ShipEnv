f8e8ae1defa1cc6fae63da8a705d49d41af5bdba
woongkingzzang <77230231+woongkingzzang@users.noreply.github.com>
2022-03-24 16:51:11 +0900

    Add files via upload

---
+++ register_test.py
@@ -0,0 +1,20 @@
+from gym.envs.registration import register
+import gym
+from gym_env import ShipEnv
+
+register(
+    id='ShipEnv-v0',
+    entry_point='gym_env:ShipEnv',
+)
+
+env = gym.make('ShipEnv-v0')
+# env = gym.make('MountainCar-v0')
+env.reset()
+
+for _ in range(1000):
+    env.render()
+    env.step(env.action_space.sample())
+env.close()
+
+
+

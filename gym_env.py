bd58bc76b2692e5e3f882f973b683636f880c947
daewoongkim <>
2022-04-06 10:28:21 +0900

    좌표계,각도 디버깅

--- gym_env.py
+++ gym_env.py
@@ -3,6 +3,7 @@ from this import d
 
 from turtle import position
 from typing import Optional
+from defusedxml import DTDForbidden
 
 import numpy as np
 import pygame
@@ -16,7 +17,12 @@ from gym.utils import seeding
 from random import randrange # random.randrange(a,b) : a이상 b미만
 
 
-
+'''
+디버깅을 위한 논리적 접근
+1. 시각화 => 도메인 초기화, 레고 블럭 쌓는 식으로 위에다 쌓기 => 완료
+2. 운동 모델 => 모델 식 비교
+3. RPM
+'''
 
 class ShipEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 60}
@@ -55,14 +61,14 @@ class ShipEnv(gym.Env):
     논문 5.1 부분을 확인해보면 됨
     자선: WAM-V 16
     *** Dynamics 수식 ***
-    u_dot = -1.091 * u + 0.0028 * T_x (x방향 가속도)
+    u_dot = -1.091 * u + 0.0028 * T_x 
     v_dot = 0.0161 * v - 0.0052 * r + 0.0002 * T_n
     r_dot = 8.2861 * v - 0.9860 * r + 0.0307 * T_n
     
     차후 원하는 실험 선박에 맞춰 바꿔야함
     '''
     
-    def __init__(self, velocity = 1.5):
+    def __init__(self):
         # screen
         self.screen_width = 1300
         self.screen_height = 800
@@ -76,39 +82,38 @@ class ShipEnv(gym.Env):
         self.max_position_y = self.screen_height
         self.position_x = self.screen_width/2
         self.position_y = self.screen_height - 30
+        self.psi = math.pi/2    # rad
+        
         # 자선의 속도
         self.min_speed = 0
         self.max_speed = 2
-        self.velocity = velocity
+        self.velocity = 0
         
         # 자선의 각도
-       
-        self.angle = 0
         self.min_angle = -90
         self.max_angle = 90
         
         # action 및 step function에서 쓰일 각도, 속도 변환을 위한 Dynamics
-        self.dt = 1
+        self.dt = 0.1
         
-        # self.T_x = 100 # 자선의 추력
-        # self.T_n = 50 # 자선의 추력
+        # 자선의 Beam
+        self.beam = 2.5
         
         # 가속도와 속도 초기화
-        self.u = 0 # 속도
-        self.v = 0
-        self.r = 0  # d_deg/dt
-        
-        self.u_dot = 0
-        self.v_dot = 0
-        self.r_dot = 0
-        
-        
+        # Local coordinate
+        self.x, self.y, self.angle = 0, 0, 0
+        self.u, self.v, self.r = 0, 0, 0 # self.r: d_deg/dt
+        self.u_dot, self.v_dot, self.r_dot = 0, 0, 0
+
+        # Rotated coordinate
+        self.X, self.Y = 0, 0
+
         self.low = np.array([self.min_position_x, self.min_position_y, self.min_speed, self.min_angle], dtype=np.float32)
         self.high = np.array([self.max_position_x, self.max_position_y, self.max_speed, self.max_angle], dtype=np.float32)
         
         # Goal
-        self.goal_x = self.screen_width /2
-        self.goal_y = 30
+        self.goal_x = self.screen_width - 1200
+        self.goal_y = self.screen_height/2
         
         # Path
         #self.path 
@@ -116,13 +121,13 @@ class ShipEnv(gym.Env):
         self.clock  = None
         self.isopen = True
     
-        
         self.action_space = spaces.Discrete(3)
         self.observation_space = spaces.Box(self.low, self.high, dtype=np.float32)
-        
+
     def step(self, action):
         
         '''
+        
         물리 모델애 대한 정의를 해놓은 부분 from 희수형님
         
         질문사항
@@ -139,45 +144,40 @@ class ShipEnv(gym.Env):
         
         '''
         
-        
-        # 추후 action에 따라 Tx, Tn 정의
-        if action == 0:
-            action_Tx = 200
-            action_Tn = 0
-        
-        elif action ==1:
-            action_Tx = 0
-            action_Tn = 10
-        
-        elif action ==2:
-            action_Tx = 50
-            action_Tn = 20
-            
+        T_l = 10
+        T_r = 10.0001
 
+        action_Tx = T_l +  T_r
+        action_Tn = (T_l - T_r) * self.beam / 2
+        
         self.r_dot = 8.2681 * self.v - 0.9860 * self.r + 0.0307 * action_Tn
         self.v_dot = 0.0161 * self.v - 0.0052 * self.r + 0.0002 * action_Tn
-        self.u_dot = -1.091 * self.u + 0.0028 * action_Tx
+        self.u_dot = -1.0191 * self.u + 0.0028 * action_Tx
 
+        # 선체고정좌표계
         self.r += self.r_dot * self.dt
         self.v += self.v_dot * self.dt
         self.u += self.u_dot * self.dt
 
-        # local
-        angle = self.r * self.dt
-        y = self.v * self.dt
-        x = self.u * self.dt
+        self.angle += self.r * self.dt  # deg
+        self.y += self.v * self.dt
+        self.x += self.u * self.dt
 
-        # global
-        self.angle += angle
-        self.position_x += x * math.cos(self.angle) - y * math.sin(self.angle)
-        self.position_y += x * math.sin(self.angle) + y * math.cos(self.angle)
+        # Rotated coordinate
+        self.psi += self.angle * 180 / math.pi # deg to rad
+        self.X = self.x * math.cos(self.psi) - self.y * math.sin(self.psi)
+        self.Y = self.x * math.sin(self.psi) + self.y * math.cos(self.psi)
+
+        # Global position
+        self.position_x += self.X
+        self.position_y += self.Y
 
         self.velocity = math.sqrt(math.pow(self.u, 2) + math.pow(self.v, 2))
 
         done = bool(self.position_x == self.goal_x and self.position_y == self.goal_y)
         reward = - 1.0  # mountain car에서 일단 가져옴
 
-        self.state = (self.position_x, self.position_y, self.velocity, self.angle)
+        self.state = (self.position_x, self.position_y, self.velocity, self.psi)
 
         return np. array(self.state, dtype=np.float32), reward, done, {}
      
@@ -200,57 +200,68 @@ class ShipEnv(gym.Env):
         
         '''
         pygame좌표계를 우주현 교수님 좌표계와 통일
-        
+        x축: 오른쪽으로 +y
+        y축: 아래쪽으로 +x 
         '''
+        
         screen_width = self.screen_width
         screen_height = self.screen_height
         
-        
-        # world_width = self.max_position_x - self.min_position_x
-        # scale = screen_width/world_width
-    
+        # height 와 width의 위치를 바꿔준 이유 = 선박 이미지 좌표와 pygame 이미지 좌표를 맞춰주기 위해
         if self.screen is None:
             pygame.init()
-            self.screen = pygame.display.set_mode((screen_width, screen_height))
+            self.screen = pygame.display.set_mode((screen_height, screen_width))
         if self.clock is None:
             self.clock = pygame.time.Clock()
         
-        self.surf = pygame.Surface((screen_width, screen_height))
+        self.surf = pygame.Surface((screen_height, screen_width))
         self.surf.fill((255, 255, 255))
         
         
          # Path 그리기
-        pygame.draw.line(self.surf, (0,0,255),[screen_width/2,0],[screen_width/2,screen_height],3)
+        pygame.draw.line(self.surf, (0,0,255),[screen_height/2,0],[screen_height/2,screen_width],3)
         
         # Goal 그리기
-        pygame.draw.circle(self.surf, (255,0,0), (self.goal_x, 770), 15)
-        
+        pygame.draw.circle(self.surf, (255,0,0), (self.goal_y, self.goal_x), 15)
+
         # 자선 그리기
+        
         '''
         
         *** 이 부분이 pygame과 우주현 교수님 논문 좌표를 통일한 부분 ***
         중요한 점은 따로 함수를 통한 회전이 아닌 center에서 좌표를 변환하여 주었다는 점
         
+        문제점 발견: pygame창에서는 좌표계 변환이 이루어지지 않음
+        
         '''
-        center = (self.state[1] - 120, -self.state[0] + 1200)
-        scale =8
+        
+        center = (self.state[1] - 370 , -self.state[0]+ 1600 )
+        scale = 8
         self.os_img: pygame.Surface = pygame.image.load("./self_ship.png")
+
+        '''
+        이미지 대신 막대기 또는 점으로 확인 => 중심점만 계속 표시하는 것으로 확정
+        '''
+        
         self.ship_size = [i//scale for i in self.os_img.get_size()]
         self.os_img: pygame.Surface = pygame.transform.scale(self.os_img, self.ship_size)
+
         # rotate
-        self.os_img = pygame.transform.rotate(self.os_img, - self.state[3])
+        self.os_img = pygame.transform.rotate(self.os_img, - self.state[3] * 180 / math.pi)
+        
         # rotate된 이미지를 덮어씌우기
-        self.img: pygame.Surface = pygame.transform.scale(self.os_img, self.ship_size)
-        self.rect = self.img.get_rect()
+        self.rect = self.os_img.get_rect()
         self.rect.center = center
 
         # 창 뒤집기
-        self.surf = pygame.transform.flip(self.surf, False, True)
+        # self.surf = pygame.transform.flip(self.surf, False, True) # 좌표계를 뒤집는데, 보여지는 것만 뒤집어짐 
+        pygame.draw.circle(self.surf, (255, 50,50), center, 5) # 뒤집고 나서 그려야됨
+
         # blit : 이미지 덮어씌우기 => 업데이트
         self.screen.blit(self.surf,(0,0))
         self.screen.blit(self.os_img, self.rect)
 
-        print(center)
+        print("center, self.state[3]: ", center, self.state[3])
         
         if mode == "human":
             self.clock.tick(self.metadata["render_fps"])
@@ -268,13 +279,7 @@ class ShipEnv(gym.Env):
             pygame.quit()
         self.isopen = False
     
-    # def rotate_angle(self, angle):
-    #     if 0 <= angle <= 90:
-    #         angle1 = angle + 270
-    #     else:
-    #         angle1 = angle - 90
-        
-        # return angle1
+   
     def rad2deg(self,rad):
         deg = 180 / math.pi * rad
         return deg
